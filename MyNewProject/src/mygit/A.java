b0ec9e5ebf5a02482d431581d5d4d78107205dab
SH <SH@DESKTOP-8S4D08K>
2024-12-28 16:23:45 +0900

    Change in devC branch

--- MyNewProject/src/mygit/A.java
+++ MyNewProject/src/mygit/A.java
@@ -4,5 +4,6 @@ package mygit;
 public class A {
 	public static void main(String[] args) {
 		System.out.println("This is A.java initial version.");
+		System.out.println("A.java is replaced in devC branch.");
  }
 }
--- MyNewProject/src/mygit/C.java
+++ MyNewProject/src/mygit/C.java
@@ -4,5 +4,6 @@ package mygit;
 public class C {
 	public static void main(String[] args) {
 		System.out.println("This is C.java initial version.");
+		System.out.println("C.java is replaced in devC branch.");
 	}
 }
